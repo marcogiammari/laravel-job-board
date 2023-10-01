6dc542790f711163849a5d0a94e180f1b4ca815c
Marco Giammari <marcogiammari@gmail.com>
2023-10-01 16:52:59 +0200

    job page: job card & link button components

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    @foreach ($jobs as $job)
+        {{-- le classi sugli slot funzionano solo se il componente ha $attributes->class --}}
+        <x-job-card class="mb-4" :$job>
+            <div>
+                <x-link-button :href="route('jobs.show', $job)">
+                    Show
+                </x-link-button>
+            </div>
+        </x-job-card>
+    @endforeach
+</x-layout>
