4aaf64cd72e47257e481f60271ec8e34ffa5794c
Marco Giammari <marcogiammari@gmail.com>
2023-10-01 21:12:27 +0200

    filtering jobs: tailwind forms plugin & text inputs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,5 +1,25 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
+
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                {{-- nb: qui non usiamo i due punti
+                i due punti servono per interpretare una espressione PHP
+                in questo caso vogliamo passare delle semplici stringhe --}}
+                <x-text-input name="search" value="" placeholder="Search for any text" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex">
+                    <x-text-input name="min_salary" value="" placeholder="From" />
+                    <x-text-input name="max_salary" value="" placeholder="To" />
+                </div>
+            </div>
+        </div>
+        </div>
+    </x-card>
     @foreach ($jobs as $job)
         {{-- le classi sugli slot funzionano solo se il componente ha $attributes->class --}}
         <x-job-card class="mb-4" :$job>
