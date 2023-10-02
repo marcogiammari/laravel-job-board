b78f85bfc12241a1700d877c74b5a8ef2e898bea
Marco Giammari <marcogiammari@gmail.com>
2023-10-02 17:22:52 +0200

    employer: factory & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Employer;
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(300)->create();
+        $users = User::all()->shuffle();
+
+        for ($i = 0; $i < 20; $i++) {
+            Employer::factory()->create([
+                // pop estrae e cancella uno user da $user
+                // in questo modo siamo sicuri che non ci siano più employer associati allo stesso user 
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            Job::factory()->create([
+                // qui usiamo random() perché va bene che un employer offra più lavori
+                'employer_id' => $employers->random()
+            ]);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
@@ -21,6 +43,5 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Job::factory(100)->create();
     }
 }
