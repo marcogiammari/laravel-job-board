92739e6e20ef5446bc0d55825e3e65c0f5e25636
Marco Giammari <marcogiammari@gmail.com>
2023-10-02 18:06:34 +0200

    employer: other employer jobs on the job page

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -46,7 +46,8 @@ class JobController extends Controller
      */
     public function show(Job $job)
     {
-        return view('jobs.show', ['job' => $job->load('employer')]);
+        // eager loading dei dati dell'employer e dei jobs in relazione ad esso
+        return view('jobs.show', ['job' => $job->load('employer.jobs')]);
     }
 
     /**
