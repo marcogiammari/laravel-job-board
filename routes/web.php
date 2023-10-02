5e18f8b8598f4a30187e82bb3a6ad247ba9b8274
Marco Giammari <marcogiammari@gmail.com>
2023-10-02 20:11:50 +0200

    authentication: the controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,8 @@ Route::get('', fn () => to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)
     ->only(['index', 'show']);
+
+
+// creiamo una rotta login a cui rimanda laravel per convenzione se si tenta di fare azioni protette da middleware di autenticazione senza essere autenticati
+Route::get('login', fn () => to_route('auth.create'))->name('login');
+Route::resource('auth', AuthController::class)->only(['create', 'store']);
