f7aeb07d543e08b99afa39b7c6686cbefeb701eb
Marco Giammari <marcogiammari@gmail.com>
2023-10-02 12:52:11 +0200

    refactor time: plain vanilla javascript to alpine.js

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,8 +1,9 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
 
-    <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET" id="filtering-form">
+    {{-- aggiungiamo l'attributo x-data per far diventare la card un alpine component --}}
+    <x-card x-data="" class="mb-4 text-sm">
+        <form x-ref="filters" action="{{ route('jobs.index') }}" method="GET" id="filtering-form">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 {{-- 1 --}}
                 <div>
@@ -11,16 +12,16 @@
                     i due punti servono per interpretare una espressione PHP
                     in questo caso vogliamo passare delle semplici stringhe --}}
                     <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text"
-                        form-id="filtering-form" />
+                        form-ref="filters" />
                 </div>
                 {{-- 2 --}}
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
                         <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From"
-                            form-id="filtering-form" />
+                            form-ref="filters" />
                         <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To"
-                            form-id="filtering-form" />
+                            form-ref="filters" />
                     </div>
                 </div>
                 {{-- 3 --}}
