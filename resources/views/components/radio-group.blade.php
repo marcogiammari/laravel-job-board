c0a5d83bd9331eff8011d926a5c8ffcbdfe7ec4b
Marco Giammari <marcogiammari@gmail.com>
2023-10-04 13:10:43 +0200

    employer: adding jobs form

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,15 +1,17 @@
 <div>
-    <label for="{{ $name }}" class="mb-1 flex items-center">
-        <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) />
-        <span class="ml-2">All</span>
-    </label>
+    @if ($allOption)
+        <label for="{{ $name }}" class="mb-1 flex items-center">
+            <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) />
+            <span class="ml-2">All</span>
+        </label>
+    @endif
 
     {{-- usiamo il metodo del component WithLabels come variabile --}}
     {{-- ora che è un array associativo usiamo il label per gestire la maiuscola --}}
     {{-- nb: esempio a scopo didattico, bastava usare Str::ucfirst() --}}
     @foreach ($optionsWithLabels as $label => $option)
         <label for="{{ $name }}" class="mb-1 flex items-center">
-            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request($name)) />
+            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === ($value ?? request($name))) />
             <span class="ml-2">{{ $label }}</span>
         </label>
     @endforeach
